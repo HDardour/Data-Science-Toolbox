5682cece0fd28a8037d594affbf4b934ebe47148
claire <CPhilippeau@fourpoints.nl>
2020-09-24 11:00:36 +0200

    update

--- demo.R
+++ demo.R
@@ -3,4 +3,7 @@
 ##dplyr heeft verschillende functies zoals filter, select, summarise
 ##dataframe
 head(mtcars)
-select(mpg, cyl, hp)
+select(mtcars, mpg, cyl, hp)
+##library(tidyr)
+x <- c(44, NA, 5, NA)
+x * 3
